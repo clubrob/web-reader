ed4865912670acfcdcbea9c4f3aab1fe7d7389c4
Rob Lindsey <rob@roblindsey.com>
2018-07-20 17:53:10 -0400

    added homepage featured news

--- src/js/controllers/homeController.js
+++ src/js/controllers/homeController.js
@@ -1,9 +1,25 @@
 const Home = (function() {
-  const homeView = require('../views/home-view.js');
+  // const endpoint =
+  //   'https://us-central1-web-reader-api.cloudfunctions.net/reader';
+  const endpoint = 'http://localhost:5001/web-reader-api/us-central1/reader';
 
   return {
     indexView: function() {
-      return homeView;
+      const homeView = require('../views/home-view.js');
+      const homeTagView = require('../views/clip-list-view.js');
+
+      return fetch(`${endpoint}/featured`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'get',
+        mode: 'cors',
+      })
+        .then(res => res.json())
+        .then(res => {
+          return homeView + homeTagView(res);
+        })
+        .catch(err => console.error(err.message));
     },
   };
 })();
